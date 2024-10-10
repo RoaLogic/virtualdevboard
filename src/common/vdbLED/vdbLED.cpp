99a8a3faf19c87b51385ca23f79da7b2f9291383
Richard Herveille <richard.herveille@roalogic.com>
2024-10-10 15:14:44 -0700

    Added vdbLED

---
+++ src/common/vdbLED/vdbLED.cpp
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//    Virtual Devboard LED Verilator C++ wrapper                   //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//    Copyright (C) 2024 Roa Logic BV - www.roalogic.com           //
+//    Copyright (C) 2024 richard.herveille@roalogic.com            //
+//                                                                 //
+//     Redistribution and use in source and binary forms, with     //
+//   or without modification, are permitted provided that the      //
+//   following conditions are met:                                 //
+//   1. Redistributions of source code must retain the above       //
+//      copyright notice, this list of conditions and the          //
+//      following disclaimer.                                      //
+//   2. Redistributions in binary form must reproduce the above    //
+//      copyright notice, this list of conditions and the          //
+//      following disclaimer in the documentation and/or other     //
+//      materials provided with the distribution.                  //
+//   3. Neither the name of the copyright holder nor the names     //
+//      of its contributors may be used to endorse or promote      //
+//      products derived from this software without specific       //
+//      prior written permission.                                  //
+//                                                                 //
+//     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND      //
+//   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,   //
+//   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF      //
+//   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE      //
+//   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR         //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE     //
+//   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+
+#include "vdbLED.hpp"
+
+/**
+ * @bried LED-On DPI-C callback
+ * @details This function gets called when the LED turns on
+ * 
+ */
+void vdbLedOn()
+{
+    //get instance scope
+    svScope scope = svGetScope();
+
+    //get hierarchical name
+    const char* scopeName = svGetNameFromScope(scope);
+
+    //report progress
+    INFO << "LED(" << scopeName << ") On\n";
+}
+
+/**
+ * @bried LED-Off DPI-C callback
+ * @details This function gets called when the LED turns off
+ * 
+ */
+void vdbLedOff()
+{
+    //get instance scope
+    svScope scope = svGetScope();
+
+    //get hierarchical name
+    const char* scopeName = svGetNameFromScope(scope);
+
+    //report progress
+    INFO << "LED(" << scopeName << ") Off\n";
+}
+
+
