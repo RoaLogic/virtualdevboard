29366bc0b95c401e9ddbb7d68b23ae529b8bed71
Richard Herveille <richard.herveille@roalogic.com>
2024-12-29 11:54:56 -0800

    Initial commit

---
+++ src/common/vdb/wxWidgetsVdbBase.hpp
@@ -0,0 +1,134 @@
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//    WX widgets virtual Devboard Base C++ header file             //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//    Copyright (C) 2024 Roa Logic BV - www.roalogic.com           //
+//    Copyright (C) 2024 richard.herveille@roalogic.com            //
+//                                                                 //
+//     Redistribution and use in source and binary forms, with     //
+//   or without modification, are permitted provided that the      //
+//   following conditions are met:                                 //
+//   1. Redistributions of source code must retain the above       //
+//      copyright notice, this list of conditions and the          //
+//      following disclaimer.                                      //
+//   2. Redistributions in binary form must reproduce the above    //
+//      copyright notice, this list of conditions and the          //
+//      following disclaimer in the documentation and/or other     //
+//      materials provided with the distribution.                  //
+//   3. Neither the name of the copyright holder nor the names     //
+//      of its contributors may be used to endorse or promote      //
+//      products derived from this software without specific       //
+//      prior written permission.                                  //
+//                                                                 //
+//     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND      //
+//   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,   //
+//   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF      //
+//   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE      //
+//   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR         //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE     //
+//   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+
+#ifndef WX_WIDGETS_VDB_BASE_HPP
+#define WX_WIDGETS_VDB_BASE_HPP
+
+#include <wx/wxprec.h>
+#include <wx/wx.h>
+#include "wx/event.h"
+#include <wx/graphics.h>
+
+#include "gui_interface.hpp"
+#include "wxGuiDistance.hpp"
+
+namespace RoaLogic {
+    using namespace observer;
+    using namespace vdb;
+namespace GUI {
+
+    /**
+     * @class cWXVdbBase
+     * @author Richard Herveille
+     * @brief virtual development board component base class
+     * 
+     * @details
+     * This class is the base for all VDB GUI components
+     */
+    class cWXVdbBase : public cGuiVDBComponent, public wxWindow
+    {
+        protected:
+        void*      _information; //Device specific information
+
+        /**
+         * @brief notify function from the vdb component
+         * @details This function receives events from the component it is registered to.
+         * @note this function runs in the verilated context.
+         */
+        virtual void notify(eEvent aEvent, void* data) {}
+
+	/**
+         * @brief Handle the event
+         * @details This function handles the wxEVT_LED event
+         * @note This function runs in the GUI thread
+         */
+        virtual void onEvent(wxCommandEvent& event) {}
+
+        public:
+	/**
+	 * @brief Constructor
+	 */
+        cWXVdbBase(cVDBCommon* myVDBComponent, distancePoint position, wxWindow* windowParent, void* Information) :
+            cGuiVDBComponent(myVDBComponent, position),
+            wxWindow(windowParent, wxID_ANY, wxDistancePoint(position, windowParent), wxDefaultSize, wxTRANSPARENT_WINDOW),
+            _information(Information)
+        {
+        }
+
+	/**
+	 * @brief Destructor
+	 */
+        ~cWXVdbBase() {}
+
+        /**
+         * @brief Component size
+         * @details Returns the size of the LED
+         */
+        virtual distanceSize GetDeviceSize() const { return distanceSize(0,0); }
+
+        /**
+         * @brief Default component size
+         * @details Returns the default size for the component.
+	 *
+            The optional @a win argument is new since wxWidgets 3.1.3 and allows to
+            get a per-monitor DPI specific size.
+         */
+        virtual wxSize GetDefaultSize(wxWindow* win = NULL)
+        {
+            if (win == NULL) { win = this; }
+            return wxDistanceSize(GetDeviceSize(), win);
+        }
+
+        /**
+	 * @brief Paint the widget
+	 * @details This function paints the widget. The 'LED' is turned on when _status=TRUE
+	 *          the corresponding bits in @_value are set, otherwise the corresponding LEDs are turned off
+	 */
+        virtual void OnPaint(wxPaintEvent& event) {}
+    };
+
+}}
+
+#endif
