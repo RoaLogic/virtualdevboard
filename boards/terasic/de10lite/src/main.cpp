e2a371839be8a5e1843de5f23ebf8f8e903a62fa
Bjorn Schouteten <b_schouteten@hotmail.com>
2024-12-29 21:35:03 +0100

    Update documentation and adjust readme file

--- boards/terasic/de10lite/src/main.cpp
+++ boards/terasic/de10lite/src/main.cpp
@@ -43,6 +43,19 @@
 //                                                                 //
 /////////////////////////////////////////////////////////////////////
 
+/**
+ * @mainpage Virtual development board
+ * 
+ * This is the software documentation for the virtual development board. The virtual development board 
+ * is a framework for creating virtual development boards. A virtual development board is an 
+ * FPGA/ASIC development board that is entirely emulated on a PC. The FPGA/ASIC logic is converted 
+ * into cycle accurate C++ models using Verilator. The peripherals (other components) on the board 
+ * are emulated using graphical components (e.g. LEDs, 7-segment Display, VGA), 
+ * abstracted (e.g. SDRAM memories), or by accessing the host PCs hardware and OS resources directly 
+ * (e.g. UART, ethernet, ...).
+ * 
+ */
+
 #include "de10lite.hpp"
 
 #include <noValueOption.hpp>
